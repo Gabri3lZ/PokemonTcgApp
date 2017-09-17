558082442377e466390f3c2dcb29bf7b1fea1a40
Gabri3lZ <gabriel.zimmerli@gmail.com>
2017-09-17 20:11:13 +0200

    close image on scroll up / scroll down

--- src/pages/image/image.ts
+++ src/pages/image/image.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import {IonicPage, NavController, NavParams, ViewController} from 'ionic-angular';
+import {AfterViewInit, Component, OnDestroy, ViewChild} from '@angular/core';
+import {Content, IonicPage, NavController, NavParams, ViewController} from 'ionic-angular';
+import {Subscription} from "rxjs/Subscription";
 
 @IonicPage({
   name: 'image-page'
@@ -8,16 +9,44 @@ import {IonicPage, NavController, NavParams, ViewController} from 'ionic-angular
   selector: 'page-image',
   templateUrl: 'image.html',
 })
-export class ImagePage {
+export class ImagePage implements AfterViewInit, OnDestroy {
 
+  @ViewChild(Content)
+  content: Content;
   imageUrl: string;
+
+  private scrollStartSubscription: Subscription;
+  private scrollEndSubscription: Subscription;
 
   constructor(public viewCtrl: ViewController, public navCtrl: NavController, public navParams: NavParams) {
     this.imageUrl = navParams.get('imageUrl');
   }
 
+  ngAfterViewInit() {
+    let scrollTopStart;
+    this.scrollStartSubscription = this.content.ionScrollStart.subscribe((event) => {
+      scrollTopStart = event.scrollTop;
+    });
+    this.scrollEndSubscription = this.content.ionScrollEnd.subscribe((event) => {
+      let scrollTopEnd = event.scrollTop;
+      let scrollDiff = Math.abs(scrollTopEnd - scrollTopStart);
+      if (scrollDiff > 100) {
+        this.close();
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.scrollStartSubscription.unsubscribe();
+    this.scrollEndSubscription.unsubscribe();
+  }
+
   close() {
     this.viewCtrl.dismiss();
+  }
+
+  test(event) {
+    console.log('event: ', event);
   }
 
 }
