ea726ed4f27564e8882b2502c682b66b4431b637
Gabri3lZ <gabriel.zimmerli@gmail.com>
2017-09-19 23:14:17 +0200

    add animation from loading page to tabs page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {Platform} from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {Nav, Platform} from 'ionic-angular';
 import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
 import {CardsProvider} from "../providers/cards/cards";
@@ -8,6 +8,7 @@ import {CardsProvider} from "../providers/cards/cards";
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
   rootPage: any = 'loading-page';
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen,
@@ -20,9 +21,12 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       this.cardsProvider.init().then(() => {
-        this.rootPage = 'tabs-page';
         this.statusBar.styleDefault();
         this.splashScreen.hide();
+        this.nav.setRoot('tabs-page', {}, {
+          animate: true,
+          animation: 'md-transition'
+        });
       });
     });
   }
