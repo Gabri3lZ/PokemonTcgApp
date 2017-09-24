b4cb370e2c280f51d36f21da2f30851c38379399
Gabri3lZ <gabriel.zimmerli@gmail.com>
2017-09-24 22:15:11 +0200

    fix set image download and display for ios

--- src/providers/cards/cards.ts
+++ src/providers/cards/cards.ts
@@ -28,8 +28,6 @@ export class CardsProvider {
   public init(): Promise<null> {
     this.fileTransfer = this.transfer.create();
     this.storageDirectory = this.file.dataDirectory;
-    console.log('storageDirectory: ', this.storageDirectory);
-    console.log('init');
     return new Promise((resolve, reject) => {
       this.initSets().then((sets: Set[]) => {
         let counter = 0;
@@ -47,7 +45,6 @@ export class CardsProvider {
   }
 
   public initSets(): Promise<Set[]> {
-    console.log('initSets');
     return new Promise((resolve, reject) => {
       this.getSetsFromStorage().then((sets: Set[]) => {
         if (sets) {
@@ -62,7 +59,6 @@ export class CardsProvider {
   }
 
   public initCards(setCode: string): Promise<Card[]> {
-    console.log('initCards');
     return new Promise((resolve, reject) => {
       this.getCardsFromStorage(setCode).then((cards: Card[]) => {
         if (cards) {
@@ -77,7 +73,6 @@ export class CardsProvider {
   }
 
   public storeSets(): Promise<Set[]> {
-    console.log('storeSets');
     return new Promise((resolve, reject) => {
       this.loadSets().then((sets: Set[]) => {
         let counter = 0;
@@ -85,13 +80,17 @@ export class CardsProvider {
           this.downloadFileWithFallback(set.imageUrls, 'sets/' + set.code + '-image.png').then((imageEntry: FileEntry) => {
             if (imageEntry) {
               set.imageEntry = imageEntry.toURL();
-              console.log('imageEntry: ', set.imageEntry);
+              if (this.platform.is('ios')) {
+                set.imageEntry = set.imageEntry.substring(7, set.imageEntry.length);
+              }
             }
             this.downloadFile(set.symbolUrl, 'sets/' + set.code + '-symbol.png').then((symbolEntry: FileEntry) => {
               counter++;
               if (symbolEntry) {
                 set.symbolEntry = symbolEntry.toURL();
-                console.log('symbolEntry: ', set.symbolEntry);
+                if (this.platform.is('ios')) {
+                  set.symbolEntry = set.symbolEntry.substring(7, set.symbolEntry.length);
+                }
               }
               if (counter === sets.length) {
                 this.storage.set('sets', sets);
@@ -105,78 +104,75 @@ export class CardsProvider {
   }
 
   public storeCards(setCode: string): Promise<Card[]> {
-    console.log('storeCards');
     return new Promise((resolve, reject) => {
       this.loadCards(setCode).then((cards: Card[]) => {
         this.storage.set(setCode, cards);
         resolve(cards);
+      }, (error) => {
+        reject(error);
       });
     });
   }
 
   public getSetsFromStorage(): Promise<Set[]> {
-    console.log('getSetsFromStorage');
     return this.storage.get('sets');
   }
 
   public getSetFromStorage(setCode: string): Promise<Set> {
-    console.log('getSetFromStorage');
     return new Promise((resolve, reject) => {
       this.getSetsFromStorage().then((sets: Set[]) => {
         let set = sets.find((set: Set) => {
           return set.code === setCode;
         });
         resolve(set);
+      }, (error) => {
+        reject(error);
       });
     });
   }
 
   public getCardsFromStorage(setCode: string): Promise<Card[]> {
-    console.log('getCardsFromStorage');
     return this.storage.get(setCode);
   }
 
   public getCardFromStorage(setCode: string, cardId: string): Promise<Card> {
-    console.log('getCardFromStorage');
     return new Promise((resolve, reject) => {
       this.getCardsFromStorage(setCode).then((cards: Card[]) => {
         let card = cards.find((card: Card) => {
           return card.id === cardId;
         });
         resolve(card);
+      }, (error) => {
+        reject(error);
       });
     });
   }
 
   public loadSets(): Promise<Set[]> {
-    console.log('loadSets');
     return new Promise((resolve, reject) => {
-      console.log('loadSets start');
       this.http.get(this.setsUrl).map((res: Response) => {
-        console.log('loadSets successful');
         return res.json().sets.reverse().map((set) => new Set(set));
       }).subscribe((sets: Set[]) => {
-        console.log('loadSets resolve');
         resolve(sets);
       }, (error) => {
-        console.log('loadSets error');
+        resolve(null);
       });
     });
   }
 
   public loadSet(setCode: string): Promise<Set> {
-    console.log('loadSet');
     return new Promise((resolve, reject) => {
       this.http.get(this.setsUrl + '/' + setCode).map((res: Response) => {
         return new Set(res.json().set)
       }).subscribe((set: Set) => {
         resolve(set);
+      }, (error) => {
+        resolve(null);
       });
     });
   }
 
   public loadCards(setCode: string): Promise<Card[]> {
-    console.log('loadCards');
     return new Promise((resolve, reject) => {
       this.http.get(this.cardsUrl, {
         params: {
@@ -189,23 +185,25 @@ export class CardsProvider {
         })
       }).subscribe((cards: Card[]) => {
         resolve(cards);
+      }, (error) => {
+        resolve(null);
       });
     });
   }
 
   public loadCard(cardId: string): Promise<Card> {
-    console.log('loadCard');
     return new Promise((resolve, reject) => {
       this.http.get(this.cardsUrl + '/' + cardId).map((res) => {
         return new Card(res.json().card)
       }).subscribe((card: Card) => {
         resolve(card);
+      }, (error) => {
+        resolve(null);
       });
     });
   }
 
   public downloadFile(url: string, path: string): Promise<FileEntry> {
-    console.log('downloadFile');
     return new Promise((resolve, reject) => {
       this.fileTransfer.download(url, this.storageDirectory + path).then((entry) => {
         resolve(entry);
@@ -216,13 +214,10 @@ export class CardsProvider {
   }
 
   public downloadFileWithFallback(urls: string[], path: string): Promise<FileEntry> {
-    console.log('downloadFileWithFallback');
     return new Promise((resolve, reject) => {
       this.fileTransfer.download(urls.shift(), this.storageDirectory + path).then((entry) => {
-        console.log('downloadFileWithFallback success: ', entry);
         resolve(entry);
       }, (error) => {
-        console.log('downloadFileWithFallback error: ', error);
         if (urls.length > 0) {
           this.downloadFileWithFallback(urls, path).then((entry: FileEntry) => {
             resolve(entry);
